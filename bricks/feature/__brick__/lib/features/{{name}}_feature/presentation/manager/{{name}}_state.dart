a2a14868c80e1f12c309d22834c33727c8ac3d7b
basicFlutter <farshid3745@gmail.com>
2024-11-01 16:54:37 +0330

    fix useCase

--- bricks/feature/__brick__/lib/features/{{name}}_feature/presentation/manager/{{name}}_state.dart
+++ bricks/feature/__brick__/lib/features/{{name}}_feature/presentation/manager/{{name}}_state.dart
@@ -7,7 +7,7 @@ class {{name.pascalCase()}}State  {
 
   const {{name.pascalCase()}}State({required  this.{{useCase.camelCase()}}Status});
 
-    {{name.pascalCase()}}State copyWith({{{event.pascalCase()}}Status? new{{useCase.pascalCase()}}Status}){
+    {{name.pascalCase()}}State copyWith({{{useCase.pascalCase()}}Status? new{{useCase.pascalCase()}}Status}){
     return {{name.pascalCase()}}State(
     {{useCase.camelCase()}}Status : new{{useCase.pascalCase()}}Status ?? {{useCase.camelCase()}}Status,
     );
