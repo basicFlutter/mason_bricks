ef1cbda26ae4b0a2df08c7c3f8ead48314f4ccf8
basicFlutter <farshid3745@gmail.com>
2024-11-01 16:27:20 +0330

    fix imports

--- bricks/feature/__brick__/lib/features/{{name}}_feature/domain/use_cases/{{useCaseName}}.dart
+++ bricks/feature/__brick__/lib/features/{{name}}_feature/domain/use_cases/{{useCaseName}}.dart
@@ -5,12 +5,12 @@ import '../../../../core/success_response/SuccessResponse.dart';
 import '../../../../core/use_case/base_use_case.dart';
 import '../repositories/{{name}}_repository.dart';
 
-class {{useCaseName.pascalCase()}}UseCase extends BaseUseCase<SuccessResponse , params>{
+class {{useCaseName.pascalCase()}}UseCase extends BaseUseCase<SuccessResponse , void>{
 
   {{name.pascalCase()}}Repository {{name}}Repository;
   {{useCaseName.pascalCase()}}UseCase({required this.{{name}}Repository });
   @override
   Future<Either<ResponseError, SuccessResponse>> call(params params)async{
-    return await {{name}}Repository.{{useCaseName}}(params);
+    return await {{name}}Repository.{{useCaseName.camelCase()}}(params);
   }
 }
