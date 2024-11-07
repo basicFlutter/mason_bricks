7e9a56c1ea75eb4055256ae03d7bf9a723715a94
basicFlutter <farshid3745@gmail.com>
2024-11-07 21:06:52 +0330

    add page

--- bricks/feature/__brick__/lib/features/{{name}}_feature/data/data_sources/{{name}}_api_provider.dart
+++ bricks/feature/__brick__/lib/features/{{name}}_feature/data/data_sources/{{name}}_api_provider.dart
@@ -2,8 +2,8 @@ import '../../../../core/data/network/api_provider.dart';
 
 class {{name.pascalCase()}}ApiProvider {
 
-  // Future<dynamic> callYourRoute() async{
-  //   Final res = await ApiProvider().get('/api/v1/project/get_suggestion_projects');
+  // Future<dynamic> call{{useCase.camelCase()}}Route() async{
+  //   final res = await ApiProvider().get('/api/v1/project/get_suggestion_projects');
   //   return res ;
   // }
 
--- bricks/feature/__brick__/lib/features/{{name}}_feature/domain/use_cases/{{useCase}}_use_case.dart
+++ bricks/feature/__brick__/lib/features/{{name}}_feature/domain/use_cases/{{useCase}}_use_case.dart
@@ -7,10 +7,10 @@ import '../repositories/{{name}}_repository.dart';
 
 class {{useCase.pascalCase()}}UseCase extends BaseUseCase<SuccessResponse , void>{
 
-  {{name.pascalCase()}}Repository {{name}}Repository;
-  {{useCase.pascalCase()}}UseCase({required this.{{name}}Repository });
+  {{name.pascalCase()}}Repository {{name.camelCase()}}Repository;
+  {{useCase.pascalCase()}}UseCase({required this.{{name.camelCase()}}Repository });
   @override
   Future<Either<ResponseError, SuccessResponse>> call(void params)async{
-    return await {{name}}Repository.{{useCase.camelCase()}}();
+    return await {{name.camelCase()}}Repository.{{useCase.camelCase()}}();
   }
 }
