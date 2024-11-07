a6b98b149818f153c5e66f6ec76b60238404713a
basicFlutter <farshid3745@gmail.com>
2024-11-07 20:58:42 +0330

    add page

---
+++ bricks/feature/__brick__/lib/features/{{name}}_feature/presentation/pages/{{name}}_page.dart
@@ -0,0 +1,11 @@
+import 'package:tmanagix/library.dart';
+class {{name.pascalCase()}} extends StatelessWidget {
+  const {{name.pascalCase()}}({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomBody(
+    body:SizedBox()
+    );
+  }
+}
