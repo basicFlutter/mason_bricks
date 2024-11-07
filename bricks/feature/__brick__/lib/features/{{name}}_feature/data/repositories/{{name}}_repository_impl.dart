6c3b0486aeb8b8e3cda033d8a87b7bb0e564bbb7
basicFlutter <farshid3745@gmail.com>
2024-11-07 20:02:31 +0330

    add api provider to repImpl

--- bricks/feature/__brick__/lib/features/{{name}}_feature/data/repositories/{{name}}_repository_impl.dart
+++ bricks/feature/__brick__/lib/features/{{name}}_feature/data/repositories/{{name}}_repository_impl.dart
@@ -7,7 +7,8 @@ import '../../../../core/use_case/base_use_case.dart';
 import '../../domain/repositories/{{name}}_repository.dart';
 import '../../../../core/data/error_handling/error_handling.dart';
 class {{name.pascalCase()}}RepositoryImpl extends {{name.pascalCase()}}Repository{
-
+    {{name.pascalCase()}}ApiProvider {{name.camelCase()}}ApiProvider;
+    {{name.pascalCase()}}RepositoryImpl({required this.{{name.camelCase()}}ApiProvider});
     @override
     Future<Either<ResponseError, SuccessResponse>> {{useCase.camelCase()}}() async{
        try {
