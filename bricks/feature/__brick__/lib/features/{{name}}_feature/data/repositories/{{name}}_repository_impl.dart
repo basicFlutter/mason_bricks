ce707ca96ad77e5b23591983f35d990f581c3f13
basicFlutter <farshid3745@gmail.com>
2025-01-27 19:44:36 +0330

    remove dio import from repo impl

--- bricks/feature/__brick__/lib/features/{{name}}_feature/data/repositories/{{name}}_repository_impl.dart
+++ bricks/feature/__brick__/lib/features/{{name}}_feature/data/repositories/{{name}}_repository_impl.dart
@@ -1,12 +1,11 @@
 import 'package:dartz/dartz.dart';
-import 'package:dio/dio.dart';
-
 import '../../../../core/exceptions/response_error.dart';
 import '../../../../core/success_response/SuccessResponse.dart';
 import '../../../../core/use_case/base_use_case.dart';
 import '../../domain/repositories/{{name}}_repository.dart';
 import '../../../../core/data/error_handling/error_handling.dart';
 import '../data_sources/{{name}}_api_provider.dart';
+
 class {{name.pascalCase()}}RepositoryImpl extends {{name.pascalCase()}}Repository{
     {{name.pascalCase()}}ApiProvider {{name.camelCase()}}ApiProvider;
     {{name.pascalCase()}}RepositoryImpl({required this.{{name.camelCase()}}ApiProvider});
