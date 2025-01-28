990210a082ce8a2657a61ded083831378801837c
basicFlutter <farshid3745@gmail.com>
2025-01-28 12:14:27 +0330

    fix feature page

--- bricks/bloc_classes/__brick__/lib/application/dealership_panel/{{name}}_bloc/{{name}}_event.dart
+++
@@ -1,9 +0,0 @@
-part of '{{name}}_bloc.dart';
-
-abstract class {{name.pascalCase()}}Event extends Equatable {}
-
-class {{event.pascalCase()}} extends {{name.pascalCase()}}Event{
-
- @override
- List<Object?> get props => [];
-}
