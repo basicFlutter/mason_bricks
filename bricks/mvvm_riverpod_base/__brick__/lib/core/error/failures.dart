60a920b0613c2fc057f0cc202ce5254921a984f2
basicFlutter <farshid3745@gmail.com>
2025-03-21 10:47:36 +0330

    add mvvm reverpod base

--- bricks/mvvm_riverpod_base/__brick__/lib/core/error/failures.dart
+++ bricks/mvvm_riverpod_base/__brick__/lib/core/error/failures.dart
@@ -1,12 +1,12 @@
 import 'package:equatable/equatable.dart';
 
 abstract class Failure extends Equatable {
-  final List properties;
+  final List<dynamic> properties;
 
   const Failure([this.properties = const <dynamic>[]]);
 
   @override
-  List<Object?> get props => [properties];
+  List<Object?> get props => properties;
 }
 
 class ServerFailure extends Failure {
