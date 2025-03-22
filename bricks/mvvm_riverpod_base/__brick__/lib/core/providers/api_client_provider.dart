9b8c86bccacd3b99f3b4691cdbb602bafc350080
basicFlutter <farshid3745@gmail.com>
2025-03-22 14:59:25 +0330

    add mvvm

--- bricks/mvvm_riverpod_base/__brick__/lib/core/providers/api_client_provider.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../network/api_client.dart';
-
-final apiClientProvider = Provider<ApiClient>((ref) {
-  return DioClient.instance;
-}); 
