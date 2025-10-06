2830b8e8b4cb071f04ead716306d66a8a68c88a9
Abbas dehghany <76767131+basicFlutter@users.noreply.github.com>
2025-10-06 09:00:57 +0330

    Update library.dart

--- bricks/base/__brick__/lib/library.dart
+++ bricks/base/__brick__/lib/library.dart
@@ -3,7 +3,8 @@ library tmanagix;
 
 export 'package:flutter/material.dart';
 export 'package:flutter_bloc/flutter_bloc.dart';
-export 'package:flutter_gen/gen_l10n/app_localizations.dart';
+//export 'package:flutter_gen/gen_l10n/app_localizations.dart';
+export  'l10n/app_localizations.dart';
 export 'package:flutter_screenutil/flutter_screenutil.dart';
 export '../core/navigation/app_navigation.dart';
 export '../core/service_locator.dart';
