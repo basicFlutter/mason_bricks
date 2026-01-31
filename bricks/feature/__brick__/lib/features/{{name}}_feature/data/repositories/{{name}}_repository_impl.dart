1da9af86962a44396fb5178663cd1035289db3a2
basicFlutter <farshid3745@gmail.com>
2026-01-31 10:10:01 +0330

    fix error handler and bloc state

--- bricks/feature/__brick__/lib/features/{{name}}_feature/data/repositories/{{name}}_repository_impl.dart
+++ bricks/feature/__brick__/lib/features/{{name}}_feature/data/repositories/{{name}}_repository_impl.dart
@@ -1,6 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
-import '../../../../core/error/error_handling.dart';
+import '../../../../core/error/error_handler.dart';
 import '../../../../core/error/response_error.dart';
 import '../../../../core/success_response/success_response.dart';
 import '../../domain/repositories/{{name}}_repository.dart';
@@ -15,7 +15,7 @@ class {{name.pascalCase()}}RepositoryImpl extends {{name.pascalCase()}}Repositor
        final response = await {{name.camelCase()}}ApiProvider.call{{useCase.pascalCase()}}Route();
        return Right(SuccessResponse());
        } on DioException catch (error) {
-       ResponseError responseError =  ErrorHandling().handleDioError(error, "{{useCase.camelCase()}}");
+       ResponseError responseError =  ErrorHandler.handleError(error);
        return Left(responseError);
        }
     }
